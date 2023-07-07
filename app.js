2d193094b5048c205d830765d7b8e14dc7420b35
Ernestoyoofi <94425296+ernestoyoofi@users.noreply.github.com>
2023-07-07 16:32:26 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 var video = document.querySelector("video")
 var canvas = document.querySelector("canvas")
 var select = document.querySelector("select")
-var isPlay = false;
-
-video.addEventListener("play", () => { isPlay = true })
-video.addEventListener("pause", () => { isPlay = false })
 
 setInterval(() => {
-  if(!video || !isPlay) return;
+  if(!video) return;
   canvas.width = video.clientWidth
   canvas.height = video.clientHeight
   var ctx = canvas.getContext("2d")
